bbab194bc2e2b99f6989a39843fd27337e46a1d9
maxdev <maxdevveb@gmail.com>
2024-09-06 13:43:15 +0300

    gitignore added

--- 5-languages/main.js
+++ 5-languages/main.js
@@ -17,3 +17,21 @@ switch (true) {
 	default:
 		console.log('Неизвестный язык')
 }
+
+/* switch (answer.toLowerCase()) {
+	case 'en':
+			console.log('Hello!')
+			break
+	case 'ru':
+			console.log('Привет!')
+			break
+	case 'de':
+			console.log('Gutten tag!')
+			break
+	case 'fr':
+			console.log('Bonjour!')
+			break
+
+	default:
+			console.log('Неизвестный язык')
+} */
