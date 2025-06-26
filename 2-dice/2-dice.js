4b809fef5b987b37428ccd813ebcff6ec5fca1da
maxdev <maxdevveb@gmail.com>
2025-06-26 19:00:14 +0300

    delete min dice value

--- 2-dice/2-dice.js
+++ 2-dice/2-dice.js
@@ -3,26 +3,26 @@
 D4, D6, D8, D10, D12, D16, D20 
 */
 
-function random(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min
+function random(max) {
+    return Math.floor(Math.random() * max) + 1
 }
 
 function throwDice(dType) {
-	const dTypes = {
-		d4: { min: 1, max: 4 },
-		d6: { min: 1, max: 6 },
-		d8: { min: 1, max: 8 },
-		d10: { min: 1, max: 10 },
-		d12: { min: 1, max: 12 },
-		d16: { min: 1, max: 16 },
-		d20: { min: 1, max: 20 },
-	}
-	if (!dTypes[dType]) {
-		return `${dType} нет такого типа dice`
-	}
-	const { min, max } = dTypes[dType]
-	return `Бросаем dice: ${dType}. Выпало: ${random(min, max)}`
+    const dTypes = {
+        d4: 4,
+        d6: 6,
+        d8: 8,
+        d10: 10,
+        d12: 12,
+        d16: 16,
+        d20: 20,
+    }
+    if (!dTypes[dType]) {
+        return `${dType} нет такого типа dice`
+    }
+    const max = dTypes[dType]
+    return `Бросаем dice: ${dType}. Выпало: ${random(max)}`
 }
 
 console.log(throwDice('d13')) // Нет такого типа
-console.log(throwDice('d6')) // Например, 4
+console.log(throwDice('d6')) // Например,
